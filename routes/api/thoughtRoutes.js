afcd94310d4a38e140a6fa463911ebcced262532
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-07-27 16:04:48 -0600

    boilerplate for thought/user routes; index.js added to /api

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+// /api/courses
+// router.route('/').get(getCourses).post(createCourse);
+
+// // /api/courses/:courseId
+// router
+//   .route('/:courseId')
+//   .get(getSingleCourse)
+//   .put(updateCourse)
+//   .delete(deleteCourse);
+
+module.exports = router;
